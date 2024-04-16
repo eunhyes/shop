805b780768149813ad46aeca056207b602d77bba
eunhyes <eunhyeess@gmail.com>
2024-04-16 11:13:45 +0900

    conflict 수정

--- shop/src/main/java/shop/dao/GoodsDAO.java
+++ shop/src/main/java/shop/dao/GoodsDAO.java
@@ -1,8 +1,6 @@
 package shop.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 import java.util.*;
 
 public class GoodsDAO {
@@ -13,6 +11,7 @@ public class GoodsDAO {
 		
 		Connection conn = DBHelper.getConnection();
 		
+		//TODO : 쿼리 수정, list 완성, list페이지에서 모델부분 수정 
 		String sql = "select * from goods order by create_date desc limit ?, ?";
 		
 		PreparedStatement stmt = conn.prepareStatement(sql);
@@ -25,8 +24,7 @@ public class GoodsDAO {
 			HashMap<String, Object> m = new HashMap<String, Object>();
 			m.put("goodsNo", rs.getInt("goods_no"));
 			m.put("Category", rs.getString("category"));
-			
-			//TODO : 쿼리 수정, list 완성, list페이지에서 모델부분 수정 
+			//TODO : 입력값 수정
 			
 			
 		}
